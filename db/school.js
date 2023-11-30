da82b2a31c821d44ca07c0dd3562c74ebb610630
Steven D. Kaste <steven.d.kaste@Stevens-MacBook-Pro.local>
2023-11-29 23:37:39 -0600

    created student row data for table

--- db/school.js
+++ db/school.js
@@ -3,7 +3,7 @@ const client = require("./client");
 const createSchool = async (name, city, state, zip_code, is_accredited) => {
   try {
     await client.query(`
-    INSERT INTO school (name)
+    INSERT INTO school (name, city, state, zip_code, is_accredited)
     VALUES ('${name}', '${city}', '${state}', '${zip_code}', '${is_accredited}' );
   `);
   } catch (err) {
--- db/students.js
+++ db/students.js
@@ -1,10 +1,18 @@
 const client = require("./client");
 
-const createStudent = async (name) => {
+const createStudent = async (
+  grade_level,
+  gpa,
+  name,
+  start_date,
+  graduation_date,
+  has_required_classes,
+  school_id
+) => {
   try {
     await client.query(`
-      INSERT INTO students (name)
-      VALUES ('${name}');
+      INSERT INTO students (grade_level, gpa, name, start_date, graduation_date, has_required_classes, school_id)
+      VALUES ('${grade_level}', '${gpa}', '${name}', '${start_date}, '${graduation_date}', '${has_required_classes}', '${school_id}');
     `);
   } catch (err) {
     console.log(err);
